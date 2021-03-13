07cdb6eefd1e6e742df3fbbdaa1fa6078ccdee4e
GAURANG GUPTA <75940473+Gaurang0053@users.noreply.github.com>
2021-03-13 14:18:35 +0530

    Add files via upload

---
+++ tenth.py
@@ -0,0 +1,20 @@
+
+"""
+def Divisors(n):
+    i = 1
+    while i <= n:
+        if (n % i == 0):
+            print (i)
+        i = i + 1
+print
+"The divisors of are: ",Divisors(50)
+
+lst=map(int,input().split())
+print(lst)
+"""
+
+
+
+
+
+
