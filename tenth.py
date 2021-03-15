57da80dfeb0c09b92aafc22cf499aba0843c04ec
GAURANG GUPTA <75940473+Gaurang0053@users.noreply.github.com>
2021-03-15 12:22:11 +0530

    Update tenth.py

--- tenth.py
+++ tenth.py
@@ -13,6 +13,17 @@ lst=map(int,input().split())
 print(lst)
 """
 
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+if x > y:
+    smaller = y
+else:
+    smaller = x
+for i in range(1,smaller + 1):
+    if((x % i == 0) and (y % i == 0)):
+        hcf = i
+print("The H.C.F. of", x,"and", y,"is", hcf)
+
 
 
 
