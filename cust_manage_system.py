1334067da8aef0215a0f60022eef2d317d0bb065
GAURANG GUPTA <75940473+Gaurang0053@users.noreply.github.com>
2021-03-16 12:31:03 +0530

    Add files via upload

---
+++ cust_manage_system.py
@@ -0,0 +1,42 @@
+class cutomer:
+
+ op=int(input("Please select one option from the given list\n"))
+
+print("1.Add Customer:")
+print("2.Delete Customer:")
+print("3.Show all customer:")
+print("4.Update customer:")
+print("5.Exit:")
+ch=int(input())
+while ch!=5:
+    if ch==1:
+        def addcustomer(self):
+            self.name=str(input("enter the name of customer:"))
+            self.id=int(input("enter the customer id:"))
+            self.no=int(input("enter the customer no:"))
+            self.add=str(input("enter the customer address"))
+    if ch==2:
+        def deletecustomer(self.id):
+        if(self.id==id):
+            delete(self.name,self,no,self.add)
+        else:
+            print("Customer ID not matched with any existing ID")
+
+    if ch==3:
+        def showCustomer(self):
+            print("the name of customer:",self.name)
+            print(" the customer id:",self.id)
+            print(" the customer no:",self.no)
+            print(" the customer address",self.add)
+
+
+    if ch==4:
+        def updatecustomer(self):
+            print("Customer ID : ", self.id)
+            self.name = input("Modify customer Name :")
+            self.no = input("Modify  customer number :")
+
+    if ch==5:
+       exit()
+
+
