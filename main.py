fa4766c737baa2c858bb9a5ca9e4f84b64097c61
GAURANG GUPTA <75940473+Gaurang0053@users.noreply.github.com>
2021-03-18 09:25:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+s="this is your python class"
+print(len(s))
+print(s[::])
+print(s[2::])
+print(s[:10:2])
+print(s[::-1])
+print(s[20::-1])
+print(s[20:10:-1])
+print(s[10:20:-1])
+print(s[10:-20:-1])
+
